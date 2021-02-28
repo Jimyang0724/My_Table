349e31e7011a0ca6130247f3e537de9c54b78b30
jimyang <jimyang890724@gmail.com>
2021-02-28 14:47:20 +0800

    2/28 Commit

--- app/src/main/java/com/example/mytable/MainActivity.kt
+++ app/src/main/java/com/example/mytable/MainActivity.kt
@@ -6,10 +6,17 @@ import android.app.AlertDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.Toast
+import androidx.core.view.isInvisible
+import androidx.core.view.isVisible
 import com.example.mytable.databinding.ActivityMainBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.ktx.Firebase
 import kotlin.system.exitProcess
 
@@ -22,6 +29,8 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        checkOrder()
+
         binding.check.setOnClickListener{
             if(binding.name.text.isEmpty()){
                 Toast.makeText(this,"輸入姓名", Toast.LENGTH_SHORT).show()
@@ -30,7 +39,12 @@ class MainActivity : AppCompatActivity() {
                 jumpToOderPage()
             }
         }
+
+        binding.cancelButton.setOnClickListener{
+            deleteOrder()
+        }
     }
+
 
     var exitTime:Long = 0
     override fun onBackPressed() {
@@ -45,6 +59,54 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun checkOrder() {
+        val user = FirebaseAuth.getInstance().currentUser
+        if(user == null){
+            Toast.makeText(this, "尚未登入", Toast.LENGTH_SHORT).show()
+            finish()
+        }
+
+        val ref = FirebaseDatabase.getInstance().reference.child(FirebaseAuth.getInstance().currentUser!!.uid)
+        ref.addValueEventListener(object: ValueEventListener{
+            @SuppressLint("SetTextI18n")
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                binding.cancelButton.visibility = View.VISIBLE
+                val drinkPath = dataSnapshot.child(user?.email.toString().split('@')[0])
+                if(drinkPath.child("Name").getValue() != null) {
+                    binding.order.text =
+                            "姓名:${drinkPath.child("Name").getValue()}\n" +
+                                    "\n飲料:${drinkPath.child("DrinkName").getValue()}\n" +
+                                    "\n甜度:${drinkPath.child("Sugar").getValue()}\n" +
+                                    "\n冰塊:${drinkPath.child("Ice").getValue()}"
+                }
+                else{
+                    binding.cancelButton.visibility = View.INVISIBLE
+                    binding.order.text = null
+                }
+            }
+            override fun onCancelled(databaseError: DatabaseError) {
+                // handle error
+                binding.cancelButton.visibility = View.INVISIBLE
+            }
+        })
+
+    }
+
+    private fun deleteOrder(){
+        val user = FirebaseAuth.getInstance().currentUser
+        if(user == null){
+            Toast.makeText(this, "尚未登入", Toast.LENGTH_SHORT).show()
+            finish()
+        }
+
+        val ref = FirebaseDatabase.getInstance().reference.child(FirebaseAuth.getInstance().currentUser!!.uid)
+        ref.child(user?.email.toString().split('@')[0]).removeValue()
+        binding.order.text = null
+        binding.cancelButton.visibility = View.INVISIBLE
+
+    }
+
+
     @SuppressLint("SetTextI18n")
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
@@ -52,24 +114,27 @@ class MainActivity : AppCompatActivity() {
             if (requestCode == 1 && resultCode == Activity.RESULT_OK) {
                 binding.order.text =
                         "飲料:${it.getString("drinksKey")}\n\n甜度:${it.getString("sugarKey")}\n\n冰塊:${it.getString("iceKey")}"
-                val user = FirebaseAuth.getInstance().currentUser
-                val userName = user?.email
 
-                if(userName == null){
+                val user = FirebaseAuth.getInstance().currentUser
+                if(user == null){
                     Toast.makeText(this, "尚未登入", Toast.LENGTH_SHORT).show()
                     finish()
                 }
 
-                val db = FirebaseDatabase.getInstance()
-                val dbRef = db.reference.child("a").child("b")//.child(user.toString()).setValue("a") // /${userName.toString()}/Drinks/")
+                val userID = user?.uid
+                val userMail = user?.email
 
-                val builder = AlertDialog.Builder(this)
-                builder.setMessage("${db.toString()} \n\n ${dbRef.toString()}")
-                builder.show()
+                val db = FirebaseDatabase.getInstance().reference
+                val dbRef = db.child(userID.toString()).child((userMail.toString().split('@'))[0])
 
-                dbRef.child("Name").setValue(it.getString("drinksKey"))
+                dbRef.child("Name").setValue(binding.name.text.toString())
+                dbRef.child("DrinkName").setValue(it.getString("drinksKey"))
                 dbRef.child("Sugar").setValue(it.getString("sugarKey"))
                 dbRef.child("Ice").setValue(it.getString("iceKey"))
+
+//                val builder = AlertDialog.Builder(this)
+//                builder.setMessage("${db} \n\n ${dbRef}")
+//                builder.show()
 
             }
         }
